20353adc6364f7ec4d785565736d31d92df02d44
Marcos Henrique Ronzani <marcos.ronzani@casadiconti.local>
2022-08-24 17:41:23 -0300

    alteração model categoria

--- cadastros/models.py
+++ cadastros/models.py
@@ -3,7 +3,11 @@ from django.db import models
 
 class Categorias(models.Model):
     descricao = models.CharField(max_length = 50)
-    tipo = models.BooleanField()
+    produto = models.BooleanField(default = False)
+    servico = models.BooleanField(default = False)
 
     class Meta:
         verbose_name = 'Categoria'
+
+    def __str__(self):
+        return self.descricao
