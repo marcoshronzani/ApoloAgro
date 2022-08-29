a0fb029a3746c3e1dadc5085d32c6e44a0c2db15
marcoshronzani <marcoshronzani@gmail.com>
2022-08-28 21:54:14 -0300

    domingo

--- cadastros/admin.py
+++ cadastros/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Categorias
+from .models import Categorias, Produtos, Servicos
 
 admin.site.register(Categorias)
+admin.site.register(Produtos)
+admin.site.register(Servicos)
+
--- cadastros/models.py
+++ cadastros/models.py
@@ -11,3 +11,32 @@ class Categorias(models.Model):
 
     def __str__(self):
         return self.descricao
+
+
+class Servicos(models.Model):
+    descricao = models.CharField(max_length= 50)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    observacao = models.TextField(blank=True, null=True)
+    categoria = models.ForeignKey(Categorias, on_delete=models.DO_NOTHING)
+
+    class Meta:
+        verbose_name = 'Servico'
+
+    def __str__(self):
+        return self.descricao
+
+
+class Produtos(models.Model):
+    descricao = models.CharField(max_length=50)
+    fabricante = models.CharField(max_length=50)
+    preco_venda = models.DecimalField(max_digits=10, decimal_places=2)
+    quantidade = models.IntegerField()
+    observacao = models.TextField(blank=True, null=True)
+    categoria = models.ForeignKey(Categorias, on_delete=models.DO_NOTHING)
+
+    class Meta:
+        verbose_name = 'Produto'
+
+
+    def __str__(self):
+        return self.descricao
