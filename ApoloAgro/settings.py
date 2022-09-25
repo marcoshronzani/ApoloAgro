2d406a9c3766594442b250bb3d52ef2d06695a06
marcoshronzani <marcoshronzani@gmail.com>
2022-09-25 11:23:56 -0300

    te

--- ApoloAgro/settings.py
+++ ApoloAgro/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_j+05cko%c3jqzpifbp%g+2mqu4bf68raaqp@!w#4m@8qwiv@k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['apoloagro.herokuapp.com', '127.0.0.1']
 
