c94f5b147a3034f789aec9f1b5e1633d0c33ec6b
marcoshronzani <marcoshronzani@gmail.com>
2022-09-26 22:29:48 -0300

    alterações html

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('login/', views.login, name = 'login'),
+    path('', views.login, name = 'login'),
     path('cadastro/', views.cadastro, name = 'cadastro'),
     path('valida_cadastro/', views.valida_cadastro, name = 'valida_cadastro'),
     path('validar_login/', views.validar_login, name = 'validar_login'),
