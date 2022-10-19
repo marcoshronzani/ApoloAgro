9df7f81476cf50b3b79e39b5a9ae2f2ec6986ec7
marcoshronzani <marcoshronzani@gmail.com>
2022-10-18 23:07:24 -0300

    inicio produtos

--- cadastros/urls.py
+++ cadastros/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('valida_categoria', views.valida_categoria, name = 'valida_categoria'),
     path('edita_categoria/<int:id>', views.edita_categoria, name = 'edita_categoria'),
     path('exluir_categoria/<int:id>', views.excluir_categoria, name='excluir_categoria'),
+    path('produtos/', views.produtos, name='produtos'),
 ]
