43a7cc3017bb40dd44bcd4265f286c671d5e329e
marcoshronzani <marcoshronzani@gmail.com>
2023-02-01 17:43:39 -0300

    alt tela login e css

--- ApoloAgro/settings.py
+++ ApoloAgro/settings.py
@@ -118,7 +118,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'templates/static')
 ]
-STATIC_ROOT = os.path.join('static')
+STATIC_ROOT = os.path.join('static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
