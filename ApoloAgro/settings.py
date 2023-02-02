26161070312f964c52e3336bb804dca0a92eda08
marcoshronzani <marcoshronzani@gmail.com>
2023-02-01 23:26:48 -0300

    config-whitenoise

--- ApoloAgro/settings.py
+++ ApoloAgro/settings.py
@@ -36,6 +36,7 @@ CSRF_TRUSTED_ORIGINS = ['https://apoloagro-production.up.railway.app']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
